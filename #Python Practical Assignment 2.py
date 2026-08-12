a8395e5d64946ec2e1594b4621668336d612aa3c
jaiminn2309 <jaiminkhilwani021@gmail.com>
2026-08-12 11:23:49 +0530

    Add files via upload

---
+++ #Python Practical Assignment 2.py
@@ -0,0 +1,29 @@
+#Python Practical Assignment 2
+#This program demonstrates variable,if-else,
+#comments,indentation,and snake_case naming
+
+#Task 1:Swiggy order
+item_price=250
+delivery_fee=30
+coupon_discount=20
+
+print("Item Price:",item_price)
+print("Delivery Fee:",delivery_fee)
+print("Coupon Discount:",coupon_discount)
+
+#Task 2:Flipkart delivery check
+product_price=1200
+
+if product_price>1000:
+    print("Eligible for free delivery")
+else:
+    print("Delivery charges applicable")
+
+#Task 4:IPL Match
+team_name="Gujarat Titans"
+run_scored=185
+overs_played=20
+
+print("Team Name:",team_name)
+print("Runs Scored:",run_scored)
+print("Overs Played:",overs_played)
